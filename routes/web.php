fc4dd9434e77416206633819a8847c922e7befaa
Jamie Howard <jamiehowardco@gmail.com>
2018-02-17 14:43:12 -0500

    Added redirection to interview questionnaire.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return redirect('https://docs.google.com/forms/d/e/1FAIpQLSdNJweWGdEUy4_wBTjiOWtTor5Col-enWk3aMrMGfLItVMt2w/viewform');
 });
+
+Route::get('/interview', function () {
+    return redirect('https://docs.google.com/forms/d/e/1FAIpQLSeKgJLTmciypPSFmyHMIkBDrDE-DbHEy8OSDq7geK-btEjU9Q/viewform');
+});
